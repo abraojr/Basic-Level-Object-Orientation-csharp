241a6a5375fba78895648692a5d99d2092c77a6e
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 19:05:47 -0300

    Demo of methods: Creating a method to obtain the benefits of reuse and delegation

---
+++ Methods-01/Methods-01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Methods_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the measurements of the sides of two triangles X and Y(assume valid measurements).
+              * Then, show the value of the areas of the two triangles and tell which of the two triangles has the largest area.
+              * The formula for calculating the area of a triangle from the measurements of its sides a, b and c is as follows (Heron's formula):
+                                     area = [p(p - a) * (p - b) * (p - c)] ^ 0, 5     where p = (a + b + c) / 2 */
+
+            Triangle x, y;
+            x = new Triangle();
+            y = new Triangle();
+
+            Console.WriteLine("Enter the measurements of the X triangle:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter the measurements of the Y triangle:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double AreaX = x.Area();
+
+            double AreaY = y.Area();
+
+            Console.WriteLine("Area of X: " + AreaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area of Y: " + AreaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (AreaX > AreaY)
+                Console.WriteLine("The largest area is : X");
+            else
+                Console.WriteLine("The largest area is : Y");
+        }
+    }
+}
