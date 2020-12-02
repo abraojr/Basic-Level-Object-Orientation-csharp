65410b04261b3414fdd3ed9b7028674910f418d1
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 18:51:16 -0300

    Demo of classes: Solving a problem without object orientation

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Enter the measurements of the X triangle: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter the measurements of the Y triangle:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Area of X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area of Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            if (areaX > areaY)
+            {
+                Console.WriteLine("The largest area is : X");
+            }
+            else
+            {
+                Console.WriteLine("The largest area is : Y");
+
+            }
+        }
+    }
+}
