35f2be78e895ed8c839ea1901a7311849f007741
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 21:23:10 -0300

    Demo of Static Members: Calculator class with static method

--- Static-members/Static-members/Calculator.cs
+++ Static-members/Static-members/Calculator.cs
@@ -2,12 +2,12 @@
 {
     class Calculator
     {
-        public double Pi = 3.14;
-        public double Circumference(double r)
+        public static double Pi = 3.14;
+        public static double Circumference(double r)
         {
             return 2.0 * Pi * r;
         }
-        public double Volume(double r)
+        public static double Volume(double r)
         {
             return 4.0 / 3.0 * Pi * r * r * r;
         }
--- Static-members/Static-members/Program.cs
+++ Static-members/Static-members/Program.cs
@@ -11,17 +11,15 @@ namespace Static_members
             /*Make a program to read any numerical value, and then show how much would be the value of a circumference and the volume
              * of a sphere for a radius of that value. Also inform the PI value to two decimal places.*/
 
-            Calculator calc = new Calculator();
-
             Console.Write("Enter the radius value: ");
             double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = calc.Circumference(radius);
-            double volume = calc.Volume(radius);
+            double circ = Calculator.Circumference(radius);
+            double volume = Calculator.Volume(radius);
 
             Console.WriteLine("Circumference: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("PI value: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PI value: " + Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
