7e9dd18faa598a892a3eed12e272cad683f7ce31
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-03 14:45:55 -0300

    Demo of constructors: Creating the first constructor

---
+++ Creating-a-constructor/Creating-a-constructor/Product.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace Creating_a_constructor
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        //The constructor has the same name as the class.
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+        public void AddProducts(int quantity)
+        {
+            Quantity += quantity;
+        }
+        public void RemoveProducts(int quantity)
+        {
+            Quantity -= quantity;
+        }
+        public override string ToString()
+        {
+            return Name
+            + ", U$ "
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantity
+            + " units, Total: U$ "
+            + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
