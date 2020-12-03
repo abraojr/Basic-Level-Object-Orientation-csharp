bb841c18b8187a6fd46f170958f85768e83461ee
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-03 18:35:30 -0300

    Demo of the keyword this: Some applications of the keyword this

---
+++ Keyword-This/Keyword-This/Product.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace Keyword_This
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        /*The Keyword This can be used to differentiate attributes from local variables:
+         
+       public Produto(string Name, double Price) 
+        {
+        this.Name = Name;
+        this.Price = Price;
+        Quantity = 0;
+        } */
+
+        // The Keyword This can be used to reference another constructor in a constructor:
+        public Product()
+        {
+            Quantity = 0;
+        }
+        public Product(string name, double price) : this()
+        {
+            Name = name;
+            Price = price;
+        }
+        public Product(string name, double price, int quantity) : this(name, price)
+        {
+            Quantity = quantity;
+        }
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+        public void AddProducts(int quantity)
+        {
+            Quantity += quantity;
+        }
+        public void RemoveProducts(int quantity)
+        {
+            Quantity -= quantity;
+        }
+        public override string ToString()
+        {
+            return Name
+            + ", U$ "
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantity
+            + " units, Total: U$ "
+            + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Keyword-This/Keyword-This/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Keyword_This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the data of a product in stock(name, price and quantity in stock).Then:
+              * • Show product data(name, price, quantity in stock, total value in stock)
+              * • Make a stock entry and show the product data again
+              * • Make a check in the stock and show the product data again*/
+
+            Console.WriteLine("Enter the product data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Price: U$ ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product p = new Product(name, price, 10);
+            Console.WriteLine();
+
+            Console.WriteLine("Product data: " + p);
+            Console.WriteLine();
+
+            Console.Write("Enter the number of products to be added to inventory: ");
+            int qntity = int.Parse(Console.ReadLine());
+            p.AddProducts(qntity);
+            Console.WriteLine();
+
+            Console.WriteLine("Updated data: " + p);
+            Console.WriteLine();
+
+            Console.Write("Enter the number of products to be removed to inventory: ");
+            qntity = int.Parse(Console.ReadLine());
+            p.RemoveProducts(qntity);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+        }
+    }
+}
