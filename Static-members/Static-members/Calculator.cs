4ff41358d83cb61483a6b6cfed7ea37e2072659a
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 21:16:34 -0300

    Demo of Static Members:  Calculator class with instance members

---
+++ Static-members/Static-members/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Static_members
+{
+    class Calculator
+    {
+        public double Pi = 3.14;
+        public double Circumference(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
--- Static-members/Static-members/Program.cs
+++ Static-members/Static-members/Program.cs
@@ -5,27 +5,23 @@ namespace Static_members
 {
     class Program
     {
-        static double Pi = 3.14;
+
         static void Main(string[] args)
         {
             /*Make a program to read any numerical value, and then show how much would be the value of a circumference and the volume
              * of a sphere for a radius of that value. Also inform the PI value to two decimal places.*/
 
+            Calculator calc = new Calculator();
+
             Console.Write("Enter the radius value: ");
             double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circ = Circumference(radius);
-            double volume = Volume(radius);
+
+            double circ = calc.Circumference(radius);
+            double volume = calc.Volume(radius);
+
             Console.WriteLine("Circumference: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("PI value: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-        }
-        static double Circumference(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * r * r * r;
+            Console.WriteLine("PI value: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
