6154fd8b20ff496dde04b5802adfc8ee94ad1f29
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-03 19:10:37 -0300

    Demo of Encapsulation: Manual implementation

---
+++ Encapsulation/Encapsulation/Product.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+namespace Encapsulation
+{
+    class Product
+    {
+        //Every attribute is defined as private
+        private string _name;
+        private double _price;
+        private int _quantity;
+        public Product()
+        {
+        }
+        public Product(string name, double price, int quantity)
+        {
+            _name = name;
+            _price = price;
+            _quantity = quantity;
+        }
+        //Get and Set methods are implemented for each attribute, according to business rules
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            if (name != null && name.Length > 1)
+            {
+                _name = name;
+            }
+        }
+        public double GetPrice()
+        {
+            return _price;
+        }
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+        public double TotalValueInStock()
+        {
+            return _price * _quantity;
+        }
+        public void AddProducts(int quantity)
+        {
+            _quantity += quantity;
+        }
+        public void RemoveProducts(int quantity)
+        {
+            _quantity -= quantity;
+        }
+        public override string ToString()
+        {
+            return _name
+            + ", U$ "
+            + _price.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantity
+            + " units, Total: U$ "
+            + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the data of a product in stock (name, price and quantity in stock). Then:
+              * • Show product data (name, price, quantity in stock, total value in stock)
+              * • Make a stock entry and show the product data again
+              * • Make a check in the stock and show the product data again*/
+
+            Product p = new Product("TV", 900.00, 10);
+
+            p.SetName("TV 4K");
+
+            Console.WriteLine("Product name: " + p.GetName());// OUTPUT: TV 4K
+            Console.WriteLine("Product price: U$ " + p.GetPrice());
+            Console.WriteLine("Product quantity in inventory: " + p.GetQuantity());
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.Write("Enter the number of products to be added to inventory: ");
+            int qntity = int.Parse(Console.ReadLine());
+            p.AddProducts(qntity);
+            Console.WriteLine();
+
+            Console.WriteLine("Updated data: " + p);
+            Console.WriteLine();
+
+            Console.Write("Enter the number of products to be removed to inventory: ");
+            qntity = int.Parse(Console.ReadLine());
+            p.RemoveProducts(qntity);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+        }
+    }
+}
