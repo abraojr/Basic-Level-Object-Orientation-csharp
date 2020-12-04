9d1f06094617e992ab4ea5a0fa217501048598ea
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-03 23:21:43 -0300

    Demo of Struct: Creating a Struct

---
+++ Struct/Struct/Point.cs
@@ -0,0 +1,11 @@
+﻿namespace Struct
+{
+    struct Point
+    {
+        public double X, Y;
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            //Console.WriteLine(p); (error: unassigned variable)
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+
+        }
+    }
+}
