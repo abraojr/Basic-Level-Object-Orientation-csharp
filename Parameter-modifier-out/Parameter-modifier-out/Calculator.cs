113303cb81908c5ee4a8150b121885e1644c58aa
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 02:12:50 -0300

    Demo of Parameter modifier: out

---
+++ Parameter-modifier-out/Parameter-modifier-out/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Parameter_modifier_out
+{
+    class Calculator
+    {
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Parameter-modifier-out/Parameter-modifier-out/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parameter_modifier_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int triple;
+
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
