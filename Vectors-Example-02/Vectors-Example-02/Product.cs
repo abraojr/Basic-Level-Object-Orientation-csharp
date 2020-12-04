2e7e0f27f71cd665e1ed479d91eecd8a68bee74b
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 01:13:55 -0300

    Demo of Vectors: Example 2

---
+++ Vectors-Example-02/Vectors-Example-02/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Vectors_Example_02
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Vectors-Example-02/Vectors-Example-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vectors_Example_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read an integer N and the data (name and price) of N Products. Store the N products in a vector. 
+             *Then, show the average price of the products.*/
+
+            Console.Write("Quantity of products: ");
+            int n = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Product name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+                Console.WriteLine();
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+            }
+            double avg = sum / n;
+            Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
