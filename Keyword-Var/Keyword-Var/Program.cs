b4abf9b0971732b94477306cc22b2e407aec166c
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 03:26:58 -0300

    Demo of Inference of types: Keyword var

---
+++ Keyword-Var/Keyword-Var/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Keyword_Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x); //OUTPUT: 10
+            Console.WriteLine(y); //OUTPUT: 20
+            Console.WriteLine(z); //OUTPUT: Maria
+
+        }
+    }
+}
