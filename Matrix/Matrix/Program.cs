cc226c05d2f5d7a2b474b21195562df955553fac
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 02:57:15 -0300

    Demo of matrix: Matrix and some of its properties

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrix = new double[2, 3];
+
+            Console.WriteLine(matrix.Length); //OUTPUT: 6
+            Console.WriteLine(matrix.Rank); //OUTPUT: 2
+            Console.WriteLine(matrix.GetLength(0)); //OUTPUT: 2
+            Console.WriteLine(matrix.GetLength(1)); //OUTPUT: 3
+        }
+    }
+}
