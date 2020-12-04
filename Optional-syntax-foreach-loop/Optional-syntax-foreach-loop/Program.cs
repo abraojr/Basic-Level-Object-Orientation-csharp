462bced28b23ec227e59595e3aae8171463f225c
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 02:20:56 -0300

    Demo of the optional syntax: foreach loop

---
+++ Optional-syntax-foreach-loop/Optional-syntax-foreach-loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Optional_syntax_foreach_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+            Console.WriteLine("----------------------------------------------");
+
+            //Optional syntax: foreach loop
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
