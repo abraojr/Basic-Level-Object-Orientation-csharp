2287b720d371489e4f41b0b747727d9bba39bf7e
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-03 23:39:29 -0300

    Demo of Nullable: Simple example of nullable

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault()); // OUTPUT: 0
+            Console.WriteLine(y.GetValueOrDefault()); // OUTPUT: 10
+
+            Console.WriteLine(x.HasValue); // OUTPUT: False
+            Console.WriteLine(y.HasValue); // OUTPUT: True
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else                                //OUTPUT: X is null
+                Console.WriteLine("X is null");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else                                //OUTPUT: 10
+                Console.WriteLine("Y is null");
+        }
+    }
+}
