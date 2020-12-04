f5da4ac311c0e839eede57bb9ba38d2feb584f04
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 15:42:32 -0300

    Demo of DateTime: Builders

---
+++ DateTime-builders/DateTime-builders/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTime_builders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2020, 12, 04);
+            DateTime d2 = new DateTime(2020, 12, 04, 13, 5, 58);
+            DateTime d3 = new DateTime(2020, 12, 04, 13, 5, 58, 275);
+
+            Console.WriteLine(d1); //OUTPUT: 04/12/2020 00:00:00
+            Console.WriteLine(d2); //OUTPUT: 04/12/2020 13:05:58
+            Console.WriteLine(d3); //OUTPUT: 04/12/2020 13:05:58
+        }
+    }
+}
