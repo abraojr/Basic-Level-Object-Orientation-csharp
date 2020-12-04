f4731037b98c6907769cb28dac81984aafe516f1
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 16:17:31 -0300

    Demo of TimeSpan internal representation: using Ticks

---
+++ TimeSpan-internal-representation/TimeSpan-internal-representation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimeSpan_internal_representation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+
+            Console.WriteLine(t1); //OUTPUT: 00:01:30
+            Console.WriteLine(t1.Ticks); //OUTPUT: 900000000
+
+        }
+    }
+}
