199a938473243218c2b42594863e6e4d035a29e5
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 15:48:24 -0300

    Demo of DateTime: Using DateTime Now, UtcNow and Today

---
+++ DateTime-Now-UtcNow-and-Today/DateTime-Now-UtcNow-and-Today/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTime_Now_UtcNow_and_Today
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = DateTime.UtcNow;
+            DateTime d3 = DateTime.Today;
+
+            Console.WriteLine(d1); //OUTPUT: 04/12/2020 15:45:01
+            Console.WriteLine(d2); //OUTPUT: 04/12/2020 18:45:01
+            Console.WriteLine(d3); //OUTPUT: 04/12/2020 00:00:00
+        }
+    }
+}
