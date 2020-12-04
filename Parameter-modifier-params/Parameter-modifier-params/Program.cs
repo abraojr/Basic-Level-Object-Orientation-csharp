b37e0dd06b3216502cb94319e78bbc9b8ae57fb7
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 01:57:34 -0300

    Demo of Parameter modifier: params

---
+++ Parameter-modifier-params/Parameter-modifier-params/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parameter_modifier_params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(10, 20, 30, 40);
+            int s2 = Calculator.Sum(1, 10, 100, 1000, 10000);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
