113af3e72f78d86ed7ede53a18f32cd9ac9f520f
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 03:44:44 -0300

    Demo of a alternative syntax: Ternary conditional expression

---
+++ Ternary-conditional-expression/Ternary-conditional-expression/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ternary_conditional_expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Product price: U$ ");
+            double price = double.Parse(Console.ReadLine());
+
+            double discount = (price < 20.0) ? price * 0.1 : price * 0.05;
+
+            Console.WriteLine("Discount= U$ " + discount.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
