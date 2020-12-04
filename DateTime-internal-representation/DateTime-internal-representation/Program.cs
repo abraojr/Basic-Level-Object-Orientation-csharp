26c1f8007b916c04287ab65952b36a3287b49775
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 04:05:44 -0300

    Demo of DateTime internal representation: using DateTime.Now and Ticks

---
+++ DateTime-internal-representation/DateTime-internal-representation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DateTime_internal_representation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine("Date now: " + d1); //OUTPUT: 04/12/2020 04:02:48            
+            Console.WriteLine("Ticks: " + d1.Ticks); //OUTPUT: Ticks= 637426513682832204
+        }
+    }
+}
