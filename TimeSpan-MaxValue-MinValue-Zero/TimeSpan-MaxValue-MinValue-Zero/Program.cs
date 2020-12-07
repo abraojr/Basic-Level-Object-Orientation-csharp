4f5d2dbce88ed599f01351bf859a5a15682eb8d4
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-07 17:44:49 -0300

    Demo of TimeSpan: Using MaxValue, MinValue and Zero

---
+++ TimeSpan-MaxValue-MinValue-Zero/TimeSpan-MaxValue-MinValue-Zero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimeSpan_MaxValue_MinValue_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = TimeSpan.MaxValue;
+            TimeSpan t2 = TimeSpan.MinValue;
+            TimeSpan t3 = TimeSpan.Zero;
+
+            Console.WriteLine(t1); //OUTPUT: 10675199.02:48:05.4775807
+            Console.WriteLine(t2); //OUTPUT: -10675199.02:48:05.4775808
+            Console.WriteLine(t3); //OUTPUT: 00:00:00
+        }
+    }
+}
